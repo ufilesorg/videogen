038dbf3dc88be208632cf286d0992e8cd41715e3
Mahdi Kiani <mahdikiany@gmail.com>
2024-11-14 14:46:29 +0000

    unbundle worker

---
+++ app/server/worker.py
@@ -0,0 +1,25 @@
+import asyncio
+import logging
+
+import pytz
+from apps.video.worker import update_video
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from .config import Settings
+
+irst_timezone = pytz.timezone("Asia/Tehran")
+logging.getLogger("apscheduler").setLevel(logging.WARNING)
+
+
+async def worker():
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(update_video, "interval", seconds=Settings.update_time)
+
+    scheduler.start()
+
+    try:
+        await asyncio.Event().wait()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+    finally:
+        scheduler.shutdown()
