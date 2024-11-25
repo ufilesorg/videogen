34414d4ad555eb97b774eb280518a07df5c8b538
Mahdi Kiani <mahdikiany@gmail.com>
2024-11-25 06:48:08 +0000

    remove TASK_UPDATE_TIME

--- app/server/config.py
+++ app/server/config.py
@@ -15,19 +15,16 @@ dotenv.load_dotenv()
 @dataclasses.dataclass
 class Settings(metaclass=Singleton):
     """Server config settings."""
-
-    aws_endpoint_url: str = os.getenv("AWS_ENDPOINT_URL")
-    aws_access_key: str = os.getenv("AWS_ACCESS_KEY")
-    aws_bucket_name: str = os.getenv("AWS_BUCKET_NAME")
-    aws_secret_key: str = os.getenv("AWS_SECRET_KEY")
-    fal_key: str = os.getenv("FAL_KEY")
     root_url: str = os.getenv("DOMAIN", default="http://localhost:8000")
     mongo_uri: str = os.getenv("MONGO_URI", default="mongodb://127.0.0.1:27017/")
     project_name: str = os.getenv("PROJECT_NAME", default="Imagine")
     base_dir: Path = Path(__file__).resolve().parent.parent
     base_path: str = "/v1/apps/videogen"
+
     update_time: int = 5
     page_max_limit: int = 100
+
+    fal_key: str = os.getenv("FAL_KEY")
 
     JWT_CONFIG: str = os.getenv(
         "USSO_JWT_CONFIG",
